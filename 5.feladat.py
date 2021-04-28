a484f98bbc542fc99a18a39dacb45b2068d03727
mogand2000 <78848887+mogand2000@users.noreply.github.com>
2021-04-28 13:26:32 +0200

    Add files via upload

--- 5.feladat.py
+++ 5.feladat.py
@@ -7,33 +7,45 @@ def feltoltes(tabla):
     return tabla
 def jatek(tabla):
     while (True):
+        print(xjon(tabla))
+        print(ojon(tabla))
+def xjon(tabla):
+     while True:
         x = input("X következik: ")
         x = x.split(' ')
-        for i in range(tabla.shape[0]):
-            for j in range(tabla.shape[1]):
-                if i == int(x[0]) and j == int(x[1]):
-                    tabla[i, j] = 'X'
-        print(tabla)
-        if tabla[0, 0] == "b'X'":  # NEM MUKODIK
-            print("NYERT")
+        if x[0].isdigit() and x[1].isdigit():
+            if int(x[0])>tabla.shape[0] or int(x[1])>tabla.shape[1]:
+                print("HIBA: ilyen pozíció nincs")
+            elif tabla[int(x[0]),int(x[1])]=='X' or tabla[int(x[0]),int(x[1])]=='O':
+                print("HIBA: a mező már meg lett jelölve")
+            else:
+                tabla[int(x[0]), int(x[1])] = 'X'
+                return tabla
+        else:
+            print("HIBA: érvénytelen bemenet")
+
+def ojon(tabla):
+    while True:
         o = input("O következik: ")
         o = o.split(' ')
-        for i in range(tabla.shape[0]):
-            for j in range(tabla.shape[1]):
-                if i == int(o[0]) and j == int(o[1]):
-                    tabla[i, j] = 'O'
-        print(tabla)
-        if int(x[0]) == 5:
-            print(tabla[0, 0])
-            break
-    print(tabla)
+        if o[0].isdigit() and o[1].isdigit():
+            if int(o[0]) > tabla.shape[0] or int(o[1]) > tabla.shape[1]:
+                print("HIBA: ilyen pozíció nincs")
+            elif tabla[int(o[0]), int(o[1])] == 'X' or tabla[int(o[0]), int(o[1])] == 'O':
+                print("HIBA: a mező már meg lett jelölve")
+            else:
+                tabla[int(o[0]), int(o[1])] = 'O'
+                return tabla
+        else:
+            print("HIBA: érvénytelen bemenet")
+
 try:
     meret=int(sys.argv[1])
     if meret>8 or meret<3:
         print("Ilyen méretű játéktáblán nem lehet játszani.")
-        breakpoint()                       #??
-    tabla=np.chararray((meret,meret))
+    tabla=np.empty((meret,meret),dtype=object)
     jatektabla=feltoltes(tabla)
+    print(jatektabla)
     jatek(jatektabla)
-except ValueError:
+except ValueError and meret>8:
     print("A megadott argumentum nem alakítható számmá.")
