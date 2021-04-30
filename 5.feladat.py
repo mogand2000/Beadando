e1f898b8f646de5b4989c12764db0b9c00bd7f24
mogand2000 <78848887+mogand2000@users.noreply.github.com>
2021-04-30 13:00:02 +0200

    Add files via upload

--- 5.feladat.py
+++ 5.feladat.py
@@ -8,13 +8,19 @@ def feltoltes(tabla):
 def jatek(tabla):
     while (True):
         print(xjon(tabla))
+        if nyerte(tabla)==True:
+            print("Az X játékos győzött!")
+            break
         print(ojon(tabla))
+        if nyerte(tabla) == True:
+            print("Az O játékos győzőtt!")
+            break
 def xjon(tabla):
      while True:
         x = input("X következik: ")
         x = x.split(' ')
         if x[0].isdigit() and x[1].isdigit():
-            if int(x[0])>tabla.shape[0] or int(x[1])>tabla.shape[1]:
+            if int(x[0])>tabla.shape[0]-1 or int(x[1])>tabla.shape[1]-1:
                 print("HIBA: ilyen pozíció nincs")
             elif tabla[int(x[0]),int(x[1])]=='X' or tabla[int(x[0]),int(x[1])]=='O':
                 print("HIBA: a mező már meg lett jelölve")
@@ -29,7 +35,7 @@ def ojon(tabla):
         o = input("O következik: ")
         o = o.split(' ')
         if o[0].isdigit() and o[1].isdigit():
-            if int(o[0]) > tabla.shape[0] or int(o[1]) > tabla.shape[1]:
+            if int(o[0]) > tabla.shape[0]-1 or int(o[1]) > tabla.shape[1]-1:
                 print("HIBA: ilyen pozíció nincs")
             elif tabla[int(o[0]), int(o[1])] == 'X' or tabla[int(o[0]), int(o[1])] == 'O':
                 print("HIBA: a mező már meg lett jelölve")
@@ -39,13 +45,46 @@ def ojon(tabla):
         else:
             print("HIBA: érvénytelen bemenet")
 
+def nyerte(tabla):
+    for i in range(tabla.shape[0]):
+        csakazvan=0
+        for j in range(tabla.shape[1]):
+            if tabla[i,0]==tabla[i,j] and tabla[i,0]!='#':
+                csakazvan+=1
+        if csakazvan==tabla.shape[0]:
+            return True
+
+    for j in range(tabla.shape[1]):
+        csakazvan=0
+        for i in range(tabla.shape[0]):
+            if tabla[0,j]==tabla[i,j] and tabla[0,j]!='#':
+                csakazvan+=1
+        if csakazvan==tabla.shape[0]:
+            return True
+
+    csakazvan = 0
+    for i in range(tabla.shape[0]):
+        if tabla[0,0]==tabla[i,i] and tabla[0,0]!="#":
+            csakazvan+=1
+        if csakazvan==tabla.shape[0]:
+            return True
+
+    csakazvan = 0
+    for i in range(tabla.shape[0]-1,0,-1):
+        for j in range(tabla.shape[1]):
+            if tabla[tabla.shape[0]-1,0]==tabla[i,j] and tabla[tabla.shape[0]-1,0]!="#":
+                csakazvan+=1
+            if csakazvan==tabla.shape[0]-1:
+                return True
+
 try:
     meret=int(sys.argv[1])
     if meret>8 or meret<3:
         print("Ilyen méretű játéktáblán nem lehet játszani.")
+        breakpoint()
     tabla=np.empty((meret,meret),dtype=object)
     jatektabla=feltoltes(tabla)
     print(jatektabla)
     jatek(jatektabla)
-except ValueError and meret>8:
+except ValueError:
     print("A megadott argumentum nem alakítható számmá.")
